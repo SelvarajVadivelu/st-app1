23b8ee49ddf0dff4e943b48eb7b085fdd5c56069
SelvarajVadivelu <32504711+SelvarajVadivelu@users.noreply.github.com>
2023-07-28 13:56:23 +0530

    Update st-app1.py

--- st-app1.py
+++ st-app1.py
@@ -8,6 +8,5 @@ df = pd.DataFrame(
 
 st.dataframe(df)  # Same as st.write(df)
 spectra = st.file_uploader("upload file", type={"csv", "txt"})
-if spectra is not None:
-    spectra_df = pd.read_csv(spectra)
+spectra_df = pd.read_csv(spectra)
 st.write(spectra_df)
