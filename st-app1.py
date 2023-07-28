80217f72e2181d651eb2a68a7ffaa17634d6a777
SelvarajVadivelu <32504711+SelvarajVadivelu@users.noreply.github.com>
2023-07-28 15:19:33 +0530

    Update st-app1.py

--- st-app1.py
+++ st-app1.py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 st.title("This is My App")
-df = pd.DataFrame(
-   np.random.randn(50, 20),
-   columns=('col %d' % i for i in range(20)))
-
-st.dataframe(df)  # Same as st.write(df)
 spectra = st.file_uploader("upload file", type={"csv", "txt"})
 spectra_df = pd.read_csv(spectra)
 st.write(spectra_df)
