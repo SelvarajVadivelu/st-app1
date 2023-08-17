2abbdc4f2a962df70c77cf66a1fdf2a4a4088187
SelvarajVadivelu <32504711+SelvarajVadivelu@users.noreply.github.com>
2023-08-17 13:12:52 +0530

    Update st-app1.py

--- st-app1.py
+++ st-app1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 st.title("This is My App")
-spectra = st.file_uploader("upload file", type={"csv", "txt"})
-spectra_df = pd.read_csv(spectra)
-st.write(spectra_df)
+#spectra = st.file_uploader("upload file", type={"csv", "txt"})
+#spectra_df = pd.read_csv(spectra)
+#st.write(spectra_df)
